d61f030a3d812a1d2bcae52fd0d06fe5b1d0d02f
lenaChenv <avermouth@163.com>
2022-07-21 18:56:53 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    title: 'Hello VuePress',
+    description: 'Just playing around',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            {
+                text: 'Guide',
+                ariaLabel: 'Guide',
+                items: [
+                { text: 'Javascript', link: '/guide/Javascript/' },
+                { text: 'Vue', link: '/guide/Vue/' },
+                { text: 'React', link: '/guide/React/' },
+                { text: 'Typescript', link: '/guide/Typescript/' },
+                { text: 'Webpack', link: '/guide/Webpack/' },
+                { text: 'Redux', link: '/guide/Redux/' }
+                ]
+            },
+            {
+                text: 'Languages',
+                ariaLabel: 'Language Menu',
+                items: [
+                { text: 'Chinese', link: '/language/chinese/' },
+                { text: 'Japanese', link: '/language/japanese/' }
+                ]
+            },
+            { text: 'Github', link: 'https://google.com' },
+        ],
+        sidebar: 'auto',
+        lastUpdated: 'Last Updated',
+        smoothScroll: true
+    }
+}
