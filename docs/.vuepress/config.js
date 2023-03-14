572eccc81bf88189059139cc56b1267348b8f5ff
lenaChen <avermouth@163.com>
2023-03-14 21:53:23 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     title: 'Hello VuePress',
     description: 'Just playing around',
+    base: '/note',
     themeConfig: {
         nav: [
             { text: 'Home', link: '/' },
